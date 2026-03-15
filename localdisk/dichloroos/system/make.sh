cb93affa7a8f60893ac77c2a15c85a1cee8e6d34
OsJanelas <osjanelas@gmail.com>
2026-03-14 23:49:08 -0300

    Add compilation for ports.c and math.c

--- localdisk/dichloroos/system/make.sh
+++ localdisk/dichloroos/system/make.sh
@@ -1,3 +1,5 @@
 nasm -f bin localdisk/dichloroos/system/boot.asm -o localdisk/dichloroos/system/binaries/boot.bin
 nasm -f bin localdisk/dichloroos/system/gdt/gdt.asm -o localdisk/dichloroos/system/binaries/gdt.o
 nasm -f bin localdisk/dichloroos/system/interrupts/interrupts.asm -o localdisk/dichloroos/system/binaries/interrupts.o
+gcc -m32 -c localdisk/dichloroos/system/include/ports.c -o localdisk/dichloroos/system/binaries/ports.o -ffreestanding
+gcc -m32 -c localdisk/dichloroos/system/include/math.c -o localdisk/dichloroos/system/binaries/math.o -ffreestanding
