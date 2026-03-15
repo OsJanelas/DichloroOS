c8ca433f8d3a40055866825345593c851d3fa05e
OsJanelas <osjanelas@gmail.com>
2026-03-14 23:43:30 -0300

    Update and rename aaaa.h to aaaa.sh

--- localdisk/dichloroos/system.old/aaaa.h
+++
@@ -1,3 +0,0 @@
-BYTE identifyata(BYTE channel, BYTE drive);
-BYTE readata(BYTE channel, BYTE drive, DWORD lba, WORD count, BYTE* buffer);
-BYTE writeata(BYTE channel, BYTE drive, DWORD lba, WORD count, BYTE* buffer);
