8e693fa85a102b0fe5ac4d020900687022981df3
OsJanelas <osjanelas@gmail.com>
2026-03-21 17:23:54 -0300

    Update make.sh

--- localdisk/dichloroos/system.old/make.sh
+++ localdisk/dichloroos/system.old/make.sh
@@ -1,3 +1,1 @@
-nasm -f bin localdisk/dichloroos/system/boot.asm -o localdisk/dichloroos/system/binaries/boot.bin
-nasm -f bin localdisk/dichloroos/system/gdt/gdt.asm -o localdisk/dichloroos/system/binaries/gdt.o
-nasm -f bin localdisk/dichloroos/system/interrupts/interrupts.asm -o localdisk/dichloroos/system/binaries/interrupts.o
+nasm -f bin localdisk/dichloroos/system.old/boot.asm -o localdisk/dichloroos/system/binaries/boot.bin
